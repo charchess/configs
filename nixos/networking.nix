41778d3eae079a059803769ea8f6fe11cad541db
charchess <charchess@gmail.com>
2025-07-22 21:24:12 +0200

    commit initial des config nixos

---
+++ nixos/networking.nix
@@ -0,0 +1,38 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  networking = {
+    hostName = "jade";
+    nameservers = [ "192.168.200.60" ];
+
+    vlans = {
+      vlan200 = {
+        id = 200;
+        interface = "enp1s0";
+      };
+      vlan111 = {
+        id = 111;
+        interface = "enp1s0";
+      };
+    };
+
+    interfaces = {
+      enp1s0.useDHCP = false;
+      vlan200.ipv4.addresses = [
+      {
+        address = "192.168.200.63";
+        prefixLength = 24;
+      }];
+      vlan111.ipv4.addresses = [
+      {
+        address = "192.168.111.63";
+        prefixLength = 24;
+      }];
+    };
+
+    defaultGateway = {
+      address = "192.168.200.1";
+      interface = "vlan200";
+    };
+  };
+}
