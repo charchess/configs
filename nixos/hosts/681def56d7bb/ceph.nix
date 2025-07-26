cbac14caeecda6996a6be7670d4ab40cdb5c8327
Charchess <charchess@gmail.com>
2025-07-27 00:24:12 +0200

    ceph fonctionnel, probleme d'osd

--- nixos/hosts/681def56d7bb/ceph.nix
+++ nixos/hosts/681def56d7bb/ceph.nix
@@ -1,6 +1,8 @@
 { config, pkgs, lib, ... }:
 
 {
+  networking.firewall.allowedTCPPorts = [ 3030 ];
+
   services.ceph-benaco = {
     enable = true;
     clusterName = "ceph";
@@ -11,41 +13,68 @@
     osdBindAddr          = "192.168.111.63";
     osdAdvertisedPublicAddr = "192.168.111.63";
 
-
     monitor = {
-        enable = true;
-        nodeName = "jade";
-        bindAddr = "192.168.111.63";
-        advertisedPublicAddr = "192.168.111.63";
-        initialKeyring = ../../secrets/ceph.mon.keyring;
+      enable = true;
+      nodeName = "jade";
+      bindAddr = "192.168.111.63";
+      advertisedPublicAddr = "192.168.111.63";
+      initialKeyring = ../../secrets/ceph.mon.keyring;
     };
 
     manager = {
-        enable = true;
-        nodeName = "jade";
+      enable = true;
+      nodeName = "jade";
     };
 
     mds = {
-        enable = true;
-        nodeName = "jade";
-        listenAddr = "192.168.111.63";
+      enable = true;
+      nodeName = "jade";
+      listenAddr = "192.168.111.63";
     };
 
     rgw = {
-        enable = true;
-        nodeName = "jade";
-        listenAddr = "192.168.111.63";
-        port = 8080;
+      enable = true;
+      nodeName = "jade";
+      listenAddr = "192.168.111.63";
+      port = 3030;
     };
 
     osds.jade_osd = {
-        enable = true;
-        id = 0;
-        uuid = "5d3d44a2-9ab8-4c24-a7f1-aa01dfe66778";
-        blockDevice = "/dev/sdb";
-        blockDeviceUdevRuleMatcher = ''KERNEL=="sdb"'';
-        bootstrapKeyring = ../../secrets/ceph.client.bootstrap-osd.keyring;
-        skipZap = false;
+      enable = true;
+      id = 0;
+      uuid = "5d3d44a2-9ab8-4c24-a7f1-aa01dfe66778";
+      blockDevice = "/dev/sdb";
+      blockDeviceUdevRuleMatcher = ''KERNEL=="sdb"'';
+      bootstrapKeyring = ../../secrets/ceph.client.bootstrap-osd.keyring;
+      skipZap = false;
     };
   };
+
+  # Copie la clé dans /etc/ceph/cephfs-admin.key à chaque rebuild
+  environment.etc."ceph/cephfs-admin.key" = {
+    source = ../../secrets/cephfs-admin.key;
+    mode   = "0400";
+  };
+
+  fileSystems."/data/cephfs" = {
+    device  = "192.168.111.63:6789:/";
+    fsType  = "ceph";
+    options = [
+      "name=admin"
+      "secretfile=/etc/ceph/cephfs-admin.key"
+      "_netdev"
+    ];
+  };
+systemd.services.ceph-osd-0 = {
+  description = "Ceph OSD 0";
+  wantedBy = [ "multi-user.target" ];
+  serviceConfig = {
+    Type = "simple";
+    User = "ceph";
+    Group = "ceph";
+    ExecStart = "${pkgs.ceph}/bin/ceph-osd -f --cluster ceph --id 0";
+    Restart = "always";
+    RestartSec = 5;
+  };
+};
 }
