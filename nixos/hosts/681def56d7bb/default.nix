9cde8ca78b248c29863b79ecaee82a5cac7e07f7
Charchess <charchess@gmail.com>
2025-07-26 03:34:42 +0200

    renommage des dossiers d'addresse mac sans : pour windows

---
+++ nixos/hosts/681def56d7bb/default.nix
@@ -0,0 +1,23 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, ... }:
+
+{
+  imports =
+  [
+    ./networking.nix
+    ../../modules/nfs-mount.nix
+    ../../modules/iscsi-connect.nix
+    ../../modules/chrony.nix
+    ../../modules/keepalived.nix
+#    ../../modules/docker.nix
+#    ../../modules/ceph.nix
+#    ../../modules/node-reporter.nix
+#    ../../modules/swarm-label-manager.nix
+    ../../modules/users.nix
+  ];
+
+
+}
