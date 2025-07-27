3e9c1c9388d8347bb17f600257952390443647c5
Charchess <charchess@gmail.com>
2025-07-27 05:28:15 +0200

    modif emy

--- nixos/hosts/681def4dd6a9/ceph-keyring-values.nix
+++ nixos/hosts/681def4dd6a9/ceph-keyring-values.nix
@@ -1,11 +1,13 @@
-{ config, pkgs, lib, ... }:
-
 {
-  services.ceph-keyring = {
+  imports = [
+    ../../modules/ceph-keyring.nix   # <-- ici
+  ];
+
+  services.ceph-key-ring = {
     enable  = true;
     fsid    = "4b687c5c-5a20-4a77-8774-487989fd0bc7";
-    monName = "emy";
-    monIp   = "192.168.111.63";
+    monName = "jade";
+    monIp   = "192.168.111.65";
   };
 }
 
