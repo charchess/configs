4320fafe9041bde15ec0686c68d5b4137b0673c5
Charchess <charchess@gmail.com>
2025-07-29 19:26:26 +0200

    modif locale
    modif locale ruby

--- nixos/hosts/00e14f680df8/ceph.nix
+++ nixos/hosts/00e14f680df8/ceph.nix
@@ -1,28 +1,56 @@
 { config, pkgs, lib, ... }:
 
+let
+  # Définir les paramètres du cluster Ceph une seule fois
+  cephFsid = "3541d2bd-2c7e-411c-8f9a-c1a06d79e2c4";
+  cephMonName = "ruby";
+  cephMonIp = "192.168.111.66"; # Assumer que ce noeud est 'ruby'
+
+  # Générer les keyrings Ceph en tant que package Nix
+  cephKeyrings = pkgs.callPackage ../../modules/pkgs/ceph-keyrings.nix {
+    fsid    = cephFsid;
+    monName = cephMonName;
+    monIp   = cephMonIp;
+  };
+
+in
 {
-  networking.firewall.allowedTCPPorts = [ 6789 ];
-  networking.firewall.allowedTCPPortRanges = [ { from = 6800; to = 7300; } ];
+  imports = [
+    # Le module ceph-benaco existant (ne pas toucher)
+    ../../modules/ceph-benaco.nix
+    # Le nouveau module pour déployer les keyrings générés
+    ../../modules/ceph-keyring-config.nix
+  ];
+
+  networking.firewall.allowedTCPPorts = [ 3030 ];
+
+  # Activer et configurer le déploiement des keyrings
+  services.cephKeyringConfig = {
+    enable = true;
+    cephKeyringsPackage = cephKeyrings;
+  };
 
   services.ceph-benaco = {
     enable = true;
     clusterName = "ceph";
-    fsid = "4b687c5c-5a20-4a77-8774-487989fd0bc7";
+    fsid = cephFsid; # Utiliser l'FSID défini ci-dessus
     publicNetworks = [ "192.168.111.0/24" ];
-    adminKeyring = ../../secrets/ceph.client.admin.keyring;
+    # Référencer les keyrings déployés dans /etc/ceph/
+    adminKeyring = "/etc/ceph/ceph.client.admin.keyring";
     initialMonitors = [
-      { hostname = "jade"; ipAddress = "192.168.111.63"; }
+      { hostname = "jade"; ipAddress = "192.168.111.66"; }
       { hostname = "ruby"; ipAddress = "192.168.111.66"; }
       { hostname = "emy";  ipAddress = "192.168.111.65"; }
     ];
-    osdBindAddr = "192.168.111.66";
+    osdBindAddr          = "192.168.111.66";
     osdAdvertisedPublicAddr = "192.168.111.66";
+
     monitor = {
       enable = true;
-      nodeName = "ruby";
-      bindAddr = "192.168.111.66";
-      advertisedPublicAddr = "192.168.111.66";
-      initialKeyring = ../../secrets/ceph.mon.keyring;
+      nodeName = cephMonName;
+      bindAddr = cephMonIp;
+      advertisedPublicAddr = cephMonIp;
+      initialKeyring = "/etc/ceph/ceph.mon.keyring"; # Référencer le keyring déployé
     };
 
     manager = {
@@ -43,65 +71,39 @@
       port = 3030;
     };
 
-    osds.ruby_osd = {
-      enable = true;
-      id     = 2;
-      uuid   = "e5205f83-0540-459b-90d6-58d78b7fa510";
-      blockDevice = "/dev/sdc";
-      blockDeviceUdevRuleMatcher = ''KERNEL=="sdc"'';
-      bootstrapKeyring = ../../secrets/ceph.client.bootstrap-osd.keyring;
-      skipZap = false;
+    osds = {
+      ruby_osd = {
+        enable = true;
+        id = 17;
+        uuid = "ccd67f04-8b11-4905-a749-4042331204b1";
+        blockDevice = "/dev/sdc";
+        blockDeviceUdevRuleMatcher = ''KERNEL=="sdb"'';
+        bootstrapKeyring = "/etc/ceph/ceph.client.bootstrap-osd.keyring"; # Référencer le keyring déployé
+        skipZap = false;
+      };
     };
   };
 
-  systemd.services.ceph-osd-2 = {
-    description = "Ceph OSD 2 (ruby)";
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      Type      = "simple";
-      User      = "ceph";
-      Group     = "ceph";
-      ExecStart = "${pkgs.ceph}/bin/ceph-osd -f --cluster ceph --id 2";
-      Restart   = "always";
-      RestartSec = 5;
-    };
-  };
+  # Supprimer le service OSD manuel redondant
+  # systemd.services.ceph-osd-0 = { ... }; # Suppression
 
-  systemd.services.ceph-mon-init-ruby = {
-    description = "Ensure Ceph monitor ruby is initialized (idempotent)";
-    wantedBy = [ "multi-user.target" ];
-    before = [ "ceph-mon@ruby.service" ];
+  # Consolider et corriger la configuration du montage CephFS
+  # Utiliser systemd.mounts pour un meilleur contrôle des dépendances.
+#  systemd.mounts = [{
+#    where  = "/data/cephfs";
+#    what   = "192.168.111.66:6789:/"; # Adresse d'un moniteur pour le montage
+#    type   = "ceph";
+#    # Utiliser la clé brute client.admin générée
+#    options = "name=admin,secretfile=/etc/ceph/ceph.client.admin.secret_key,_netdev";
+#    wantedBy = [ "multi-user.target" ];
+#    # S'assurer que les services Ceph sont démarrés avant le montage
+#    after = [ "network.target" "ceph-mon.service" "ceph-mds.service" ];
+#    requires = [ "network.target" "ceph-mon.service" "ceph-mds.service" ];
+#  }];
 
-    path = with pkgs; [ ceph ];
-
-    script = ''
-      set -e
-      MON_DIR="/var/lib/ceph/mon/ceph-ruby"
-      if [[ ! -f "$MON_DIR/keyring" ]]; then
-        mkdir -p "$MON_DIR"
-        cp /etc/ceph/ceph.mon.keyring "$MON_DIR/keyring"
-        chown -R ceph:ceph "$MON_DIR"
-        ceph-mon --mkfs -i ruby --public-addr 192.168.111.66 --keyring "$MON_DIR/keyring"
-      fi
-    '';
-
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-      User = "root";
-    };
-  };
-
-  systemd.mounts = [{
-    where  = "/data/cephfs";
-    what   = "192.168.111.66:6789:/";
-    type   = "ceph";
-    options = "name=admin,secretfile=/etc/ceph/ceph.client.admin.secret_key,_netdev";
-    wantedBy = [ "multi-user.target" ];
-  }];
-
-  systemd.automounts = [{
-    where  = "/data/cephfs";
-    wantedBy = [ "multi-user.target" ];
-  }];
+  # L'automount est une bonne pratique pour les partages réseau
+#  systemd.automounts = [{
+#    where  = "/data/cephfs";
+#    wantedBy = [ "multi-user.target" ];
+#  }];
 }
