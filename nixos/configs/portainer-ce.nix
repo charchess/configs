7d09a767cd7467dab0b749cfb20d5fb3c6c3d40f
charchess <charchess@gmail.com>
2025-07-31 01:06:19 +0200

    on repart sur des bases saines pour ceph ...

--- nixos/configs/portainer-ce.nix
+++
@@ -1,8 +0,0 @@
-{ lib, config, pkgs, ... }:
-
-{
-  imports = [ ../modules/portainer-ce.nix ];
-
-  services.portainer-ce.enable = true;
-  services.portainer-ce.version = "sts";
-}
--- nixos/configs/portainer-ee.nix
+++
@@ -1,8 +0,0 @@
-{ lib, config, pkgs, ... }:
-
-{
-  imports = [ ../modules/portainer-ee.nix ];
-
-  services.portainer-ee.enable = true;
-  services.portainer-ee.version = "sts";
-}
