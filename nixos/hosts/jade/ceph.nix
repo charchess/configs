dbe0f1ac97bcaa748f59fc435bb8f6ec1ad04a41
Charchess <charchess@gmail.com>
2025-08-03 02:03:04 +0200

    [Ceph] Configuration finale avec gestion des liens symboliques

---
+++ nixos/hosts/jade/ceph.nix
@@ -0,0 +1,28 @@
+{ config, lib, ... }:
+
+let
+  clusterConfig = import ../../common/ceph/cluster-config.nix { inherit lib; };
+in {
+  imports = [ 
+    ../../modules/ceph 
+    ../../common/sops.nix 
+  ];
+
+  services.ceph-custom = {
+    enable = true;
+    nodes.jade = {
+      hostname = "jade";
+      address = clusterConfig.cephCluster.nodes.jade.address;
+      roles = ["mon" "mgr"];
+    };
+    publicNetwork = "192.168.111.0/24";
+    clusterNetwork = "192.168.111.0/24";
+    bootstrapSingleNode = false;
+  };
+
+  sops.secrets."ceph/jade-keyring" = {
+    sopsFile = ./ceph-keyring-values.nix;
+    owner = "ceph";
+    group = "ceph";
+  };
+}
