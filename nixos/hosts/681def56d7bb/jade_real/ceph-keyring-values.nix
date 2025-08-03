68a39a4818b2f41b0d819c0f94e4ad54a5fdc564
Charchess <charchess@gmail.com>
2025-08-03 02:33:30 +0200

    Suppression import sops en double

---
+++ nixos/hosts/681def56d7bb/jade_real/ceph-keyring-values.nix
@@ -0,0 +1,9 @@
+{ lib }:
+{
+  "mon." = {
+    key = "AQCvzYFjAAAAABAA9UY4lBTOB0dLqM80Xpo3ZA==";
+  };
+  "mgr.jade" = {
+    key = "AQCvzYFjAAAAABAA9UY4lBTOB0dLqM80Xpo3ZA==";
+  };
+}
---
+++ nixos/hosts/681def56d7bb/jade_real/networking.nix
@@ -0,0 +1,36 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+imports =
+  [
+    ../../common/networking.nix
+  ];
+
+  networking = {
+    hostName = "jade";
+
+    vlans = {
+      vlan200 = {
+        id = 200;
+        interface = "enp1s0";
+      };
+      vlan111 = {
+        id = 111;
+        interface = "enp1s0";
+      };
+    };
+
+    interfaces = {
+      enp1s0.useDHCP = false;
+      vlan200.ipv4.addresses = [{
+        address = "192.168.200.63";
+        prefixLength = 24;
+      }];
+      vlan111.ipv4.addresses = [{
+        address = "192.168.111.63";
+        prefixLength = 24;
+      }];
+    };
+
+  };
+}
