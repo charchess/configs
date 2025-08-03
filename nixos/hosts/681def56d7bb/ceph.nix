f7db2ec51622a5dc804cdaa9ef30e964d7fe6ba9
Charchess <charchess@gmail.com>
2025-08-03 02:27:02 +0200

    Référence dynamique au host ID

--- nixos/hosts/681def56d7bb/ceph.nix
+++ nixos/hosts/681def56d7bb/ceph.nix
@@ -3,32 +3,23 @@
 let
   clusterConfig = import ../../common/ceph/cluster-config.nix { inherit lib; };
 in {
-  imports = [
-    ../../modules/ceph
-    ../../common/sops.nix
+  imports = [ 
+    ../../modules/ceph 
+    ../../common/sops.nix 
   ];
 
   services.ceph-custom = {
     enable = true;
-    
-    nodes = {
-      jade = {
-        hostname = "jade";
-        address = clusterConfig.cephCluster.nodes.jade.address;
-        roles = ["mon" "mgr"];
-      };
+    nodes.${config.services.ceph-custom.thisNode} = {
+      hostname = "jade";
+      address = clusterConfig.cephCluster.nodes.${config.services.ceph-custom.thisNode}.address;
+      roles = ["mon" "mgr"];
     };
-    
-    thisNode = "jade";
-    
     publicNetwork = "192.168.111.0/24";
     clusterNetwork = "192.168.111.0/24";
-    
-    # Désactiver le bootstrap après déploiement initial
     bootstrapSingleNode = false;
   };
 
-  # Intégration des secrets via SOPS
   sops.secrets."ceph/jade-keyring" = {
     sopsFile = ./ceph-keyring-values.nix;
     owner = "ceph";
