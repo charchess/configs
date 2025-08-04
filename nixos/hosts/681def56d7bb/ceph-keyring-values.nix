4d26c0a82e36a3531db4753cb31c4f79a0cc09b1
charchess <charchess@gmail.com>
2025-08-04 23:07:37 +0200

    ajout k3s

--- nixos/hosts/681def56d7bb/ceph-keyring-values.nix
+++
@@ -1,9 +0,0 @@
-{ lib }:
-{
-  "mon." = {
-    key = "AQCvzYFjAAAAABAA9UY4lBTOB0dLqM80Xpo3ZA==";
-  };
-  "mgr.jade" = {
-    key = "AQCvzYFjAAAAABAA9UY4lBTOB0dLqM80Xpo3ZA==";
-  };
-}
--- nixos/modules/ceph/services/osd.nix
+++
@@ -1,6 +0,0 @@
-{ config, lib, pkgs, ... }:
-
-{
-  # Module OSD vide pour l'instant - on se concentre sur le monitor
-  config = {};
-}
